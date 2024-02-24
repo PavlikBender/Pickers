af6f65cb081e6a5448ece1e7c1ef3a19c12be9ad
PavlikBender <bbbender92@gmail.com>
2024-02-24 14:47:14 +0300

    Добавьте файлы проекта.

---
+++ Structures/COMDLG_FILTERSPEC.cs
@@ -0,0 +1,18 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Pickers.Structures;
+
+[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto, Pack = 4)]
+internal struct COMDLG_FILTERSPEC
+{
+    internal COMDLG_FILTERSPEC(string name, string spec)
+    {
+        pszName = name;
+        pszSpec = spec;
+    }
+
+    [MarshalAs(UnmanagedType.LPWStr)]
+    public string pszName;
+    [MarshalAs(UnmanagedType.LPWStr)]
+    public string pszSpec;
+}
